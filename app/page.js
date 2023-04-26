a39cbaa9eaa8deb2d4d3e99fc3d08cb7079d5cd2
ProskurinV <proskyrinv.g@gmail.com>
2023-04-26 18:29:55 +0300

    start

--- app/page.js
+++ app/page.js
@@ -1,91 +1,112 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from "next/font/google";
+import styles from "./globals.css";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={inter.className}>
+      <div className="px-2 flex items-center">
+        <div className="py-4 overflow-y-auto whitespace-nowrap flex items-center">
+          <Link className="text-gray-600 hover:text-gray-900" href="#">
+            Home
+          </Link>
+          <span className="mx-2 text-gray-500">&gt;</span>
+
+          <Link className="text-gray-600 hover:text-gray-900" href="#">
+            News
+          </Link>
+          <span className="mx-2 text-gray-500">&gt;</span>
+          <Link className="text-gray-600 hover:text-gray-900" href="#">
+            Tech
+          </Link>
+          <span className="mx-2 text-gray-500">&gt;</span>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="border border-dotted"></div>
+
+      <div className="my-5">
+        <h5 className="font-bold text-lg uppercase text-gray-700 mb-2">
+          Subscribe
+        </h5>
+        <p className="text-gray-600 mb-4">Subscribe to news</p>
+        <input
+          placeholder="your email"
+          type="email"
+          className="text-gray-700 bg-gray-100 p-2 w-full border rounded-t hover:border-gray-600 "
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        <button className="px-4 py-2 bg-indigo-600 text-white rounded-b w-full hover:bg-indigo-800">
+          Subscribe
+        </button>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="border border-dotted"></div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="px-2">
+        <Link href="" className="block mb-10">
+          <div className="flex">
+            <div>
+              <h3 className="mt-3 mb-10 text-gray-700 font-bold text-2xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              </h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Similique incidunt accusamus, voluptates suscipit natus
+                consequuntur enim odit fugiat error facilis ut libero quis.
+                Nulla dolorum facilis, quaerat ut quas maxime?
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link href="" className="block mb-10">
+          <div className="flex">
+            <div>
+              <h3 className="mt-3 mb-10 text-gray-700 font-bold text-2xl">
+                Quisque vel magna vel arcu commodo tincidunt.
+              </h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Similique incidunt accusamus, voluptates suscipit natus
+                consequuntur enim odit fugiat error facilis ut libero quis.
+                Nulla dolorum facilis, quaerat ut quas maxime?
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link href="" className="block mb-10">
+          <div className="flex">
+            <div>
+              <h3 className="mt-3 mb-10 text-gray-700 font-bold text-2xl">
+                Phasellus pulvinar elit eu nulla efficitur, sit amet tincidunt
+                dui pretium.
+              </h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Similique incidunt accusamus, voluptates suscipit natus
+                consequuntur enim odit fugiat error facilis ut libero quis.
+                Nulla dolorum facilis, quaerat ut quas maxime?
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link href="" className="block mb-10">
+          <div className="flex">
+            <div>
+              <h3 className="mt-3 mb-10 text-gray-700 font-bold text-2xl">
+                Duis sed velit eget augue feugiat porttitor.
+              </h3>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Similique incidunt accusamus, voluptates suscipit natus
+                consequuntur enim odit fugiat error facilis ut libero quis.
+                Nulla dolorum facilis, quaerat ut quas maxime?
+              </p>
+            </div>
+          </div>
+        </Link>
+      </section>
     </main>
-  )
+  );
 }
